66ef2aa84347ad5580e1c328eeabe902d2246358
Leon Timmermans <fawaka@gmail.com>
2011-11-30 13:20:52 +0100

    More robust input type detection

--- lib/Bio/SFF/Reader.pm
+++ lib/Bio/SFF/Reader.pm
@@ -31,7 +31,7 @@ has _fh => (
 	},
 	coerce   => sub {
 		my $val = shift;
-		return $val if reftype($val) eq 'GLOB';
+		return $val if ref($val) and reftype($val) eq 'GLOB';
 		open my $fh, '<:raw', $val or croak "Could open file $val: $!";
 		return $fh;
 	}
